a9c70aa4bd93a609f1d5e47e684dd2397eb31a1f
Md. Mahbub Alam <mmahbub569@gmail.com>
2021-01-20 12:11:03 +0600

    minor changes

---
+++ src/app/messages/login.messages.ts
@@ -0,0 +1,4 @@
+export enum LoginMessageEN{
+    LOGIN_SUCCESS_EN = 'Logged In Successfully',
+    LOGIN_FAILED_EN = 'The credentials you entered did not match our records. Please double-check and try again.'
+}
